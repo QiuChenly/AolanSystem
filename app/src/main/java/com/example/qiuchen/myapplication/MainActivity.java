7d405d7c94ad65252e181acbf5fcf8dcbc1eadc9
QiuChenly <963084062@qq.com>
2017-04-29 12:40:43 +0800

    UI更新
    修正 输入法挡住编辑框无法看见输入内容BUG
    更新 UI

--- app/src/main/java/com/example/qiuchen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/qiuchen/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.qiuchen.myapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -28,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            //如果需要透明导航栏，请加入标记
+
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
+        }
+
+
         OkGo.init(getApplication());//TM这....
         btn = (Button) findViewById(R.id.mLogin);
         mUser = (EditText) findViewById(R.id.mUser);
--- app/src/main/java/com/example/qiuchen/myapplication/MainUser.java
+++ app/src/main/java/com/example/qiuchen/myapplication/MainUser.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.nfc.cardemulation.HostNfcFService;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -19,6 +20,8 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -68,12 +71,21 @@ public class MainUser extends AppCompatActivity implements NavigationView.OnNavi
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            //如果需要透明导航栏，请加入标记
+            //View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);//不设置统一颜色,实现彩色图标
         navigationView.setNavigationItemSelectedListener(this);
 
 
